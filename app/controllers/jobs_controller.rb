400369d774aa378f36aa6619ff899f937e600242
zengjinlin <kinglinxinsen@gmail.com>
2017-07-07 22:27:34 +0800

    implement step 5

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -10,6 +10,10 @@ class JobsController < ApplicationController
 
   def show
     @job = Job.find(params[:id])
+    if @job.is_hidden
+      flash[:warning] = "This Job already archived"
+      redirect_to root_path
+    end
   end
 
   def create
